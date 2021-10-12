afd2890b4fded92d957bbdb7097f04679f23ecc7
cinemyp <kiyamovtimur01@gmail.com>
2021-10-12 22:49:57 +0300

    сделал верстку страницы отображения диалогов

--- Screens/MessengerScreen.js
+++ Screens/MessengerScreen.js
@@ -1,22 +1,50 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text } from "react-native";
+import { ScrollView, StyleSheet, Text } from "react-native";
+import { Avatar, ListItem } from "react-native-elements";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Dialog } from "../components/Dialog";
 
 export default function MessengerScreen() {
+  const messages = [
+    {
+      id: 0,
+      avatar:
+        "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      name: "Amelia, 27",
+      message: "Let's get to your favorite restaurant.",
+    },
+    {
+      id: 1,
+      avatar:
+        "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      name: "Amelia, 27",
+      message: "Let's get to your favorite restaurant.",
+    },
+    {
+      id: 2,
+      avatar:
+        "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      name: "Amelia, 27",
+      message: "Let's get to your favorite restaurant.",
+    },
+    {
+      id: 3,
+      avatar:
+        "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      name: "Amelia, 27",
+      message: "Let's get to your favorite restaurant.",
+    },
+  ];
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Messages</Text>
+      <ScrollView style={{ height: "100%" }}>
+        {messages.map(({ id, avatar, name, message }) => (
+          <Dialog key={id} avatar={avatar} name={name} message={message} />
+        ))}
+      </ScrollView>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
